8f727da0075b45802c4239f46fac5a774ac8b04c
流星423 <72623417+ryusei423@users.noreply.github.com>
2024-06-26 23:08:00 +0800

    修复多重播放

--- static/yiyan.js
+++ static/yiyan.js
@@ -3,7 +3,7 @@
 
 function SetTitle(){
 	var httpRequest = new XMLHttpRequest();//第一步：创建需要的对象
-	httpRequest.open('POST', 'https://v1.hitokoto.cn?c=a&c=b', true); //第二步：打开连接/***发送json格式文件必须设置请求头 ；如下 - */
+	httpRequest.open('POST', 'https://international.v1.hitokoto.cn?c=a&c=b', true); //第二步：打开连接/***发送json格式文件必须设置请求头 ；如下 - */
 	httpRequest.setRequestHeader("Content-type", "application/json");//设置请求头 注：post方式必须设置请求头（在建立连接后设置请求头）var obj = { name: 'zhansgan', age: 18 };
 	
 	//这个设置参数的方法不知道为什么没有用 直接写在链接里了
