84783345f3bd9a741135e97a551de1707a246060
流星423 <72623417+ryusei423@users.noreply.github.com>
2024-09-01 10:39:43 +0800

    整点音乐

--- static/yiyan.js
+++ static/yiyan.js
@@ -14,9 +14,11 @@ function SetHitokoto(callback){
             var from_ = dataObj.from;
             document.getElementById("yiyan").innerHTML = hitokoto_;
             document.getElementById("yiyan_1").innerHTML = "--- " + from_;
-            if (typeof callback === 'function') {
-                callback();
-            }
+        }
+		
+		//告诉回调函数请求的状态
+		if (typeof callback === 'function') {
+			callback(httpRequest.readyState == 4 && httpRequest.status == 200);
         }
     };
 }
